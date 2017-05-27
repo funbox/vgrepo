b25da12fe3fd5506597dd020ea50987726a56817
Gleb Goncharov <inbox@gongled.ru>
2017-05-27 19:43:50 +0400

    #4: added reading metadata from HTTP resource by given URL.

--- metadata/meta.go
+++ metadata/meta.go
@@ -8,6 +8,7 @@ import (
 	"pkg.re/essentialkaos/ek.v9/fsutil"
 	"pkg.re/essentialkaos/ek.v9/jsonutil"
 	"pkg.re/essentialkaos/ek.v9/path"
+	"pkg.re/essentialkaos/ek.v9/req"
 
 	"github.com/gongled/vgrepo/prefs"
 )
@@ -15,7 +16,7 @@ import (
 // ////////////////////////////////////////////////////////////////////////////////// //
 
 const (
-	METADATA_EXTENSION_TYPE = ".json" // extension name of metadata file with a leading dot
+	MD_EXT_TYPE = ".json" // extension name of metadata file with a leading dot
 )
 
 // ////////////////////////////////////////////////////////////////////////////////// //
@@ -29,7 +30,7 @@ type VMetadata struct {
 
 // metaName returns name of the metadata file with extension
 func (m *VMetadata) metaName() string {
-	return fmt.Sprintf("%s%s", m.Name, METADATA_EXTENSION_TYPE)
+	return fmt.Sprintf("%s%s", m.Name, MD_EXT_TYPE)
 }
 
 // MetaDir returns directory string to metadata file
@@ -95,6 +96,32 @@ func (m *VMetadata) loadFromFile(metaPath string) (*VMetadataRepository, error) 
 	return info, err
 }
 
+// loadFromURL returns new VMetadataRepository struct which was fetched from the HTTP/HTTPS resource
+func (m *VMetadata) loadFromURL(metaURL string) (*VMetadataRepository, error) {
+	resp, err := req.Request{URL: metaURL}.Get()
+
+	if err != nil {
+		return nil, fmt.Errorf("cannot fetch metadata from URL %s", metaURL)
+	}
+
+	if resp.StatusCode != 200 {
+		return nil, fmt.Errorf(
+			"cannot fetch metadata: server return status %s",
+			resp.StatusCode,
+		)
+	}
+
+	info := &VMetadataRepository{}
+
+	err = resp.JSON(info)
+
+	if err != nil {
+		return nil, fmt.Errorf("cannot decode server response: %v", err)
+	}
+
+	return info, nil
+}
+
 // dumpToFile dumps VMetadata struct on the metadata file on the disk
 func (m *VMetadata) dumpToFile(metaPath string) error {
 	var err error
@@ -110,9 +137,13 @@ func (m *VMetadata) dumpToFile(metaPath string) error {
 	return jsonutil.EncodeToFile(metaPath, m.VMetadataRepository)
 }
 
-// ReadMeta returns new VMetadata struct which was read from the metadata file
+// ReadMeta returns new VMetadata struct which was read from the metadata file or URL
 func (m *VMetadata) ReadMeta() (*VMetadata, error) {
 	md, err := m.loadFromFile(m.MetaPath())
+
+	if err != nil {
+		md, err = m.loadFromURL(m.MetaURL())
+	}
 
 	return NewMetadata(
 		m.Preferences,
